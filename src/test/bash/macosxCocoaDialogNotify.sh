3df8eb396da37d38a76ff0243159ec8f0d315efe
Francois Wauquier <wokier@gmail.com>
2013-08-28 09:47:40 +0200

    bash tests for mac os x

---
+++ src/test/bash/macosxCocoaDialogNotify.sh
@@ -0,0 +1,1 @@
+/Applications/CocoaDialog.app/Contents/MacOS/CocoaDialog bubble --title "test title" --text "test message"
---
+++ src/test/bash/macosxTerminalNotifier.sh
@@ -0,0 +1,1 @@
+/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier -title "test title" -message "test message"
