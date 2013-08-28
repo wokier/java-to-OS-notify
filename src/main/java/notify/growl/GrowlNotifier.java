fa8b63c99abfc233a36635fbd1d68e0394296fd1
Francois Wauquier <wokier@gmail.com>
2013-08-28 09:51:11 +0200

    growl v1.3 compatibility

--- src/main/java/notify/growl/GrowlNotifier.java
+++ src/main/java/notify/growl/GrowlNotifier.java
@@ -18,105 +18,112 @@ import notify.UnableToNotifyException;
  */
 public class GrowlNotifier implements Notifier {
 
-    private static final String GROWL_APPLICATION = "GrowlHelperApp";
+	private static final String GROWL_APPLICATION = "com.Growl.GrowlHelperApp";
 
-    private static final String APPLICATION_NAME = "NOTIFY";
+	private static final String APPLICATION_NAME = "java.Notify";
 
-    private ScriptEngine appleScriptEngine;
+	private ScriptEngine appleScriptEngine;
 
-    private boolean registered;
+	private boolean registered;
 
-    public GrowlNotifier() {
-	this.appleScriptEngine = createScriptEngine();
-    }
+	public GrowlNotifier() {
+		super();
+	}
 
-    public ScriptEngine createScriptEngine() {
-	ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
-	ScriptEngine appleScriptEngine = scriptEngineManager.getEngineByName("AppleScript");
-	return appleScriptEngine;
-    }
+	public ScriptEngine createScriptEngine() {
+		ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+		return scriptEngineManager.getEngineByName("AppleScript");
+	}
 
-    public void registerApplication(String[] notifications) throws ScriptException {
-	String script = script().add("tell application ").quote(GROWL_APPLICATION).nextRow("set the availableList to ").array(notifications).nextRow("set the enabledList to ").array(notifications)
-		.nextRow("register as application ").quote(APPLICATION_NAME).add(" all notifications availableList default notifications enabledList").nextRow("end tell").get();
-	executeScript(script);
+	public void registerApplication(String[] notifications) throws ScriptException {
+		String script = script().add("tell application id ").quote(GROWL_APPLICATION).nextRow("set the availableList to ").array(notifications).nextRow("set the enabledList to ").array(notifications)
+				.nextRow("register as application ").quote(APPLICATION_NAME).add(" all notifications availableList default notifications enabledList").nextRow("end tell").get();
+		executeScript(script);
+	}
+
+	@Override
+	public boolean isSupported() {
+		try {
+            createScriptEngineIfNecessary();
+            registerIfNecessary();
+		} catch (IllegalStateException e) {
+			return false;
+		}
+		return appleScriptEngine != null && registered && isGrowlEnabled();
+	}
+
+    private void createScriptEngineIfNecessary() {
+        if (appleScriptEngine == null) {
+            this.appleScriptEngine = createScriptEngine();
+        }
     }
 
     @Override
-    public boolean isSupported() {
-	try {
-	    registerIfNecessary();
-	} catch (IllegalStateException e) {
-	    return false;
+	public void notify(MessageType messageType, String title, String message) {
+        createScriptEngineIfNecessary();
+        registerIfNecessary();
+		notify(messageType.name(), prefix(messageType) + title, message);
 	}
-	return appleScriptEngine != null && isGrowlEnabled();
-    }
 
-    @Override
-    public void notify(MessageType messageType, String title, String message) {
-	registerIfNecessary();
-	notify(messageType.name(), prefix(messageType) + title, message);
-    }
-
-    private String prefix(MessageType messageType) {
-	switch (messageType) {
-	case NONE:
-	    return "";
-	case INFO:
-	    return "? ";
-	case WARNING:
-	    return "? ";
-	case ERROR:
-	    return "? ";
+	private String prefix(MessageType messageType) {
+		switch (messageType) {
+		case NONE:
+			return "";
+		case INFO:
+			return "! ";
+		case WARNING:
+			return "∆ ";
+		case ERROR:
+			return "☠ ";
+		}
+		return null;
 	}
-	return null;
-    }
 
-    private void registerIfNecessary() {
-	if (!registered) {
-	    if (appleScriptEngine == null) {
-		throw new IllegalStateException("You are trying to notify with Growl on a non-supported OS");
-	    }
-	    try {
-		registerApplication(new String[] { "NONE", "INFO", "WARNING", "ERROR" });
-	    } catch (ScriptException e) {
-		throw new UnableToNotifyException("Unable to register Growl Application", e);
-	    }
-	    registered = true;
+	private void registerIfNecessary() {
+		if (!registered) {
+			if (appleScriptEngine == null) {
+				throw new IllegalStateException("You are trying to notify with Growl on a non-supported OS");
+			}
+			try {
+				registerApplication(new String[] { "NONE", "INFO", "WARNING", "ERROR" });
+			} catch (ScriptException e) {
+				throw new UnableToNotifyException("Unable to register Growl Application", e);
+			}
+			registered = true;
+		}
 	}
-    }
 
-    public void notify(String notificationName, String title, String message) {
-	String script = script().add("tell application ").quote(GROWL_APPLICATION).nextRow("notify with name ").quote(notificationName).add(" title ").quote(title).add(" description ").quote(message)
-		.add(" application name ").quote(APPLICATION_NAME).nextRow("end tell").get();
-	try {
-	    executeScript(script);
-	} catch (ScriptException e) {
-	    throw new UnableToNotifyException("Unable to notify with Growl", e);
+	public void notify(String notificationName, String title, String message) {
+		String script = script().add("tell application id ").quote(GROWL_APPLICATION).nextRow("notify with name ").quote(notificationName).add(" title ").quote(title).add(" description ").quote(message)
+				.add(" application name ").quote(APPLICATION_NAME).nextRow("end tell").get();
+		try {
+			executeScript(script);
+		} catch (ScriptException e) {
+			throw new UnableToNotifyException("Unable to notify with Growl", e);
+		}
 	}
-    }
 
-    private boolean isGrowlEnabled() {
-	String script = script().add("tell application ").quote("System Events").nextRow("return count of (every process whose name is ").quote(GROWL_APPLICATION).add(") > 0").nextRow("end tell")
-		.get();
-	long count = executeScript(script, 0L);
-	return count > 0;
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T> T executeScript(String script, T defaultValue) {
-	try {
-	    return (T) appleScriptEngine.eval(script, appleScriptEngine.getContext());
-	} catch (ScriptException e) {
-	    return defaultValue;
+	private boolean isGrowlEnabled() {
+		String script = script().add("tell application ").quote("System Events").nextRow("return count of (every process whose bundle identifier is ").quote(GROWL_APPLICATION).add(") > 0")
+				.nextRow("end tell").get();
+		long count = executeScript(script, 0L);
+		return count > 0;
 	}
-    }
 
-    private void executeScript(String script) throws ScriptException {
-	appleScriptEngine.eval(script, appleScriptEngine.getContext());
-    }
+	@SuppressWarnings("unchecked")
+	private <T> T executeScript(String script, T defaultValue) {
+		try {
+			return (T) appleScriptEngine.eval(script, appleScriptEngine.getContext());
+		} catch (ScriptException e) {
+			return defaultValue;
+		}
+	}
 
-    private AppleScriptBuilder script() {
-	return new AppleScriptBuilder();
-    }
+	private void executeScript(String script) throws ScriptException {
+		appleScriptEngine.eval(script, appleScriptEngine.getContext());
+	}
+
+	private AppleScriptBuilder script() {
+		return new AppleScriptBuilder();
+	}
 }
--- src/test/java/notify/growl/GrowlNotifierTest.java
+++ src/test/java/notify/growl/GrowlNotifierTest.java
@@ -2,6 +2,7 @@ package notify.growl;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
 import notify.MessageType;
 
 import org.junit.Ignore;
@@ -9,30 +10,30 @@ import org.junit.Test;
 
 public class GrowlNotifierTest {
 
-    GrowlNotifier growlNotifier = new GrowlNotifier();
+	GrowlNotifier growlNotifier = new GrowlNotifier();
 
-    @Test
-    @Ignore
-    public void testIsNotSupported() {
-	assertFalse(growlNotifier.isSupported());
-    }
+	@Test
+	@Ignore
+	public void testIsNotSupported() {
+		assertFalse(growlNotifier.isSupported());
+	}
 
-    @Test
-    // @Ignore("not a mac")
-    public void testIsSupported() {
-	assertTrue(growlNotifier.isSupported());
-    }
+	@Test
+	// @Ignore("not a mac")
+	public void testIsSupported() {
+		assertTrue(growlNotifier.isSupported());
+	}
 
-    @Test
-    // @Ignore("not a mac")
-    public void testNotifyMessageTypeStringString() throws Exception {
-	growlNotifier.notify(MessageType.NONE, "NONE", "none");
-	Thread.sleep(1000);
-	growlNotifier.notify(MessageType.INFO, "INFO", "info");
-	Thread.sleep(1000);
-	growlNotifier.notify(MessageType.WARNING, "WARNING", "warning");
-	Thread.sleep(1000);
-	growlNotifier.notify(MessageType.ERROR, "ERROR", "error");
-	Thread.sleep(1000);
-    }
+	@Test
+	// @Ignore("not a mac")
+	public void testNotifyMessageTypeStringString() throws Exception {
+		growlNotifier.notify(MessageType.NONE, "NONE", "none");
+		Thread.sleep(1000);
+		growlNotifier.notify(MessageType.INFO, "INFO", "info");
+		Thread.sleep(1000);
+		growlNotifier.notify(MessageType.WARNING, "WARNING", "warning");
+		Thread.sleep(1000);
+		growlNotifier.notify(MessageType.ERROR, "ERROR", "error");
+		Thread.sleep(1000);
+	}
 }
